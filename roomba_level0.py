3d1d03501cabb1c017cfaef0de2b47d06ada210f
blinky32123 <jkrepner2028@kehillahstudent.org>
2025-09-04 09:22:37 -0700

    roomba

--- roomba_level0.py
+++ roomba_level0.py
@@ -13,7 +13,23 @@ window = room.draw_room(level = 0)
 
 ###
 # Start your code here
- 
+forward(160)
+right(270)
+forward(160)
+right(270)
+forward(160)
+right(270)
+forward(120)
+right(270)
+forward(120)
+right(270)
+forward(80)
+right(270)
+forward(80)
+right(270)
+forward(40)
+right(270)
+forward(40)
  
  
 # End your code here
